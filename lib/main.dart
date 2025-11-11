c72ac442fe9318f8c7d493b02b911eab79c8d53a
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-10 22:09:06 -0500

    Etape 3 : question 1 -  Ajouter un container

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,22 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Column(
-            children: [Text('Hello world!'), Text('la premiere tentative...')],
+            children: [
+              Text('Hello world!'),
+              Text('la premiere tentative...'),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  'boite stylisee avec container',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ],
           ),
         ),
       ),
